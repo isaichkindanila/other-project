32fd7bdb14a164c5fb1eec10312d693f0781e42e
danila <isaichkindanila@gmail.com>
2020-04-01 18:03:40 +0300

    fix AuthService#isAuthenticated
    Signed-off-by: danila <isaichkindanila@gmail.com>

--- src/main/java/ru/itis/other/project/services/AuthService.java
+++ src/main/java/ru/itis/other/project/services/AuthService.java
@@ -2,8 +2,6 @@ package ru.itis.other.project.services;
 
 import ru.itis.other.project.util.UserInfo;
 
-import java.util.Optional;
-
 /**
  * Utility service for extracting {@link UserInfo} from current {@link org.springframework.security.core.Authentication}.
  */
@@ -13,11 +11,6 @@ public interface AuthService {
      * @return whether or not current request is authenticated
      */
     boolean isAuthenticated();
-
-    /**
-     * @return wrapped {@link UserInfo} if current request is authenticated, empty otherwise
-     */
-    Optional<UserInfo> getUserInfoOptional();
 
     /**
      * @return {@link UserInfo} associated with current request
--- src/main/java/ru/itis/other/project/services/impl/AuthServiceImpl.java
+++ src/main/java/ru/itis/other/project/services/impl/AuthServiceImpl.java
@@ -1,12 +1,11 @@
 package ru.itis.other.project.services.impl;
 
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import ru.itis.other.project.services.AuthService;
 import ru.itis.other.project.util.UserInfo;
-
-import java.util.Optional;
 
 @Service
 class AuthServiceImpl implements AuthService {
@@ -15,24 +14,15 @@ class AuthServiceImpl implements AuthService {
     public boolean isAuthenticated() {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
 
-        return auth != null && auth.isAuthenticated();
-    }
-
-    @Override
-    public Optional<UserInfo> getUserInfoOptional() {
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-
-        if (auth == null || !auth.isAuthenticated()) {
-            return Optional.empty();
-        } else {
-            return Optional.of((UserInfo) auth.getPrincipal());
-        }
+        return !(auth instanceof AnonymousAuthenticationToken);
     }
 
     @Override
     public UserInfo getUserInfo() {
-        return getUserInfoOptional().orElseThrow(
-                () -> new IllegalStateException("current request is not authenticated")
-        );
+        try {
+            return (UserInfo) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        } catch (ClassCastException e) {
+            throw new IllegalStateException("current request is not authenticated");
+        }
     }
 }
